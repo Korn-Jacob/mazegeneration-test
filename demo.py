3a768ad84734d3ca2d4a1e744b41138b9b53680c
Korn-Jacob <jackor27@bergen.org>
2024-12-29 18:41:14 -0500

    minor change, ignore it

--- demo.py
+++ demo.py
@@ -10,7 +10,7 @@ def main_page():
 @app.get("/maze")
 def show_maze():
     size = int(request.args.get('size'))
-    if size > 500 and not request.args.get('confirmation'):
+    if size > 100 and not request.args.get('confirmation'):
         return render_template("confirmation.jinja", size=size)
     return render_template('maze.jinja', board=randomized_dfs_maze(size))
 
